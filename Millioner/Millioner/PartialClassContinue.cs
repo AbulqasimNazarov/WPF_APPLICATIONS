0b7e835600f825bed810af6b7e52f6ce05602ebb
Abulqasim <abulqasim.nazarov@gmail.com>
2023-06-26 01:05:33 +0400

    Add images and Dont reapet yourself fixed

---
+++ Millioner/Millioner/PartialClassContinue.cs
@@ -0,0 +1,158 @@
+﻿
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace Millioner;
+
+public partial class MainWindow
+{
+
+    public void ELSE()
+    {
+        
+        this.questionBox.Visibility = Visibility.Hidden;
+        this.LOSEPIC.Visibility = Visibility.Visible;
+
+        foreach (var item in this.buttons)
+        {
+            item.Visibility = Visibility.Hidden;
+        }
+        this.buttonAgainStart.Visibility = Visibility.Visible;
+
+        this.questionListBox.Visibility = Visibility.Hidden;
+    }
+
+
+    public void ButtonStart_Click(object sender, RoutedEventArgs e)
+    {
+        this.questionBox.Visibility = Visibility.Visible;
+        this.questionListBox.Visibility = Visibility.Visible;
+
+        Button button = (Button)sender;
+        this.Loqopic.Visibility = Visibility.Hidden;
+        this.buttonStart.Visibility = Visibility.Hidden;
+        this.buttonAgainStart.Visibility = Visibility.Hidden;
+
+        QuestionData answers1 = new QuestionData("question1.json");
+
+        foreach (var item in this.buttons)
+        {
+            item.Visibility = Visibility.Visible;
+        }
+
+        if (answers1 != null)
+        {
+            answers1.Otrabotka(this.questionBox, this.buttons);
+            this.questionNumber = 1;
+        }
+        else
+            MessageBox.Show("ERROR");
+
+        this.questionListBox.IsEnabled = true;
+        this.questionListBox.SelectedIndex = 0;
+
+
+    }
+
+
+    private void buttonAnswer_Click(object sender, RoutedEventArgs e)
+    {
+
+        QuestionData? answers2 = new QuestionData("question2.json");
+        QuestionData? answers3 = new QuestionData("question3.json");
+        QuestionData? answers4 = new QuestionData("question4.json");
+        QuestionData? answers5 = new QuestionData("question5.json");
+        Button b = (Button)sender;  
+        switch (this.questionNumber)
+        {
+            case 1:
+                if (b.Name == "buttonA")
+                {
+                   
+                    if (answers2 != null)
+                    {
+                        answers2.Otrabotka(this.questionBox, this.buttons);
+                        this.questionListBox.SelectedIndex = 1;
+                        this.questionNumber = 2;
+                    }
+                    else
+                        MessageBox.Show("ERROR");
+                }
+                else
+                {
+                    ELSE();
+                }
+                break;
+            case 2:
+                if (b.Name == "buttonC")
+                {
+                if (answers2 != null)
+                    {                      
+                        answers3.Otrabotka(this.questionBox, this.buttons);
+                        this.questionListBox.SelectedIndex = 2;
+                        this.questionNumber = 3;
+                    }
+                    else
+                        MessageBox.Show("ERROR");
+                }
+                else
+                {
+                    ELSE();
+                }
+                break;
+            case 3:
+                if (b.Name == "buttonA")
+                {
+                    if (answers4 != null)
+                    {
+                        answers4.Otrabotka(this.questionBox, this.buttons);
+                        this.questionListBox.SelectedIndex = 3;
+                        this.questionNumber = 4;
+                    }
+                    else
+                        MessageBox.Show("ERROR");
+                }
+                else
+                {
+                    ELSE();
+                }
+                break;
+            case 4:
+                if (b.Name == "buttonD")
+                {
+                    if (answers5 != null)
+                    { 
+                        answers5.Otrabotka(this.questionBox, this.buttons);
+                        this.questionListBox.SelectedIndex = 4;
+                        this.questionNumber = 5;
+                    }
+                    else
+                        MessageBox.Show("ERROR");
+                }
+                else
+                {
+                    ELSE();
+                }
+                break;
+            case 5:
+                if (b.Name == "buttonC")
+                {
+                    ELSE();
+                    this.LOSEPIC.Visibility = Visibility.Hidden;
+                    this.buttonAgainStart.Visibility = Visibility.Hidden;
+
+                }
+                else
+                {
+                    ELSE();
+                }
+                break;
+
+            default:
+                break;
+        }
+
+    }
+}
--- Millioner/Millioner/QuestionData.cs
+++ Millioner/Millioner/QuestionData.cs
@@ -2,15 +2,18 @@
 
 using System.Collections.Generic;
 using System.IO;
+using System.Runtime.CompilerServices;
 using System.Text.Json;
+using System.Windows.Controls;
+using System.Windows.Shapes;
 
 namespace Millioner;
 
 public class QuestionData
 {
-    public List<string>? questionList = new List<string>();
+    public List<string> questionList = new List<string>();
     //public List<string> answerList = new List<string>();
-
+    QuestionData? a;
     public string? Question { get; set; }
     public string? AnswerA { get; set; }
     public string? AnswerB { get; set; }
@@ -21,11 +24,24 @@ public class QuestionData
     {
 
     }
-
-    public static QuestionData? LoadQuestion(string path)
+    public QuestionData(string path)
     {
         string json = File.ReadAllText(path);
-        return JsonSerializer.Deserialize<QuestionData>(json);
+        this.a = JsonSerializer.Deserialize<QuestionData>(json);
+        this.questionList.Add(a.AnswerA);
+        this.questionList.Add(a.AnswerB);
+        this.questionList.Add(a.AnswerC);
+        this.questionList.Add(a.AnswerD);
     }
-    
+
+    public void Otrabotka(TextBox Text, List<Button> buttonList)
+    {
+        Text.Text = a?.Question;
+        buttonList[0].Content = $"A: {a?.AnswerA}";
+        buttonList[1].Content = $"B: {a?.AnswerB}";
+        buttonList[2].Content = $"C: {a?.AnswerC}";
+        buttonList[3].Content = $"D: {a?.AnswerD}";
+    }
+
+
 }
